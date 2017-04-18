1de1bd3474709b871229c7653fe3e346a3f8de21
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-04-18 16:31:59 +0200

    Ad integration configuration.

--- config/plugins.php
+++ config/plugins.php
@@ -12,9 +12,21 @@
  */
  define('LOCAL_SITE_STATS', true);
 
- /**
+/**
  * Municipio webfont
  * Load webfonts for municipio theme.
  */
 
 define('WEB_FONT', 'Roboto');
+
+/**
+ * Integration with ad
+ * @var string
+ */
+ define('AD_INTEGRATION_URL', 'https://intranat.helsingborg.se/ad-api/');
+
+/**
+ * Integration with ad (domain used for ad-accounts)
+ * @var string
+ */
+ define('AD_USER_DOMAIN', 'helsingborg.se');
