eecda9940a90a914eb818721277c1dbd9bc2efd1
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-04 12:55:49 +0200

    Revert "Cookie domain"
    This reverts commit 5f4725c963c3e6c67291856bb7647b2325829d30.
    
    # Conflicts:
    #	config/cookie.php

--- config/cookie.php
+++ config/cookie.php
@@ -5,12 +5,8 @@
  * @var bool
  */
 
-<<<<<<< HEAD
-define('COOKIE_DOMAIN', 'helsingborg.se');
-=======
 if (strpos($_SERVER['HTTP_HOST'], "skola.dev") !== false) {
     define('COOKIE_DOMAIN', ".skola.dev");
 } else {
     define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
 }
->>>>>>> parent of c586ac7... Cookie
