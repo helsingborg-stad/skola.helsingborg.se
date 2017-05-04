7bf45c50608ee7c2176d5e5fb7346c6d1f9e468a
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-04 14:29:40 +0200

    Translated.

--- wp-content/themes/municipio-school/functions.php
+++ wp-content/themes/municipio-school/functions.php
@@ -7,6 +7,10 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
     require_once dirname(ABSPATH) . '/vendor/autoload.php';
 }
 
+add_action('after_setup_theme', function () {
+    load_theme_textdomain('municipio-school', get_template_directory() . '/languages');
+});
+
 require_once MUNICIPIOSCHOOL_PATH . 'library/Vendor/Psr4ClassLoader.php';
 $loader = new MunicipioSchool\Vendor\Psr4ClassLoader();
 $loader->addPrefix('MunicipioSchool', MUNICIPIOSCHOOL_PATH . 'library');
