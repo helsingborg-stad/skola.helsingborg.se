2faab219550cf1faacd6b9d6db375f1e771899d8
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-11-27 15:43:47 +0100

    Filter header navigation classes

--- wp-content/themes/municipio-highschool/library/Theme/Filters.php
+++ wp-content/themes/municipio-highschool/library/Theme/Filters.php
@@ -8,6 +8,34 @@ class Filters
     {
         //Filter header data
         add_filter('HbgBlade/data', array($this, 'filterHbgBladeData'), 10, 1);
+
+        //Sticky nav class
+        add_filter('Municipio/StickyScroll', array($this, 'stickyNavBarClass'));
+
+        //Nav group class
+        add_filter('Municipio/Jumbo/NavGroupClass', array($this, 'navGroupClass'));
+    }
+
+
+    /**
+     * Append navgroup class
+     * @param string $class old classname
+     * @return string new classname
+     */
+    public function navGroupClass($class)
+    {
+        $class = 'nav-group-flex';
+        return $class;
+    }
+
+    /**
+     * Append new sticky header class
+     * @return array - Containing view data
+     */
+    public function stickyNavBarClass($class)
+    {
+        $class = 'navbar-sticky';
+        return $class;
     }
 
     /**
