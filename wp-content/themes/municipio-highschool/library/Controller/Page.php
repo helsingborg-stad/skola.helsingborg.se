468985bc73127356aa87be5e4b25d5ac9b303eac
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-29 07:10:51 +0100

    Get custom_menu_title and fallback for page title

--- wp-content/themes/municipio-highschool/library/Controller/Page.php
+++ wp-content/themes/municipio-highschool/library/Controller/Page.php
@@ -9,27 +9,31 @@ class Page extends \Municipio\Controller\BaseController
 
     public function init()
     {
-
         $this->globalToLocal('post');
 
         $this->data['hasLeftSidebar'] = false;
         $this->data['contentGridSize'] = 'grid-md-7';
 
         $this->data['menuTitle'] = $this->getMenuName();
-
-
-
     }
 
     public function getTopLevelPageID()
     {
-
-        var_dump(get_post_ancestors($this->post));
-
+        return reset(get_post_ancestors($this->post));
     }
 
     public function getMenuName()
     {
-        $this->getTopLevelPageID();
+        $topLevelPageID = $this->getTopLevelPageID();
+
+        if (is_numeric($topLevelPageID) && get_post_status($topLevelPageID) == 'publish') {
+            if ($post_title = get_post_meta($topLevelPageID, 'custom_menu_title', true)) {
+                return $post_title;
+            }
+
+            return get_the_title($topLevelPageID);
+        }
+
+        return "";
     }
 }
