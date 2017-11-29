41270be13a046ca1281a846d9d91224760d55e41
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-29 07:02:26 +0100

    Get top level page id

--- wp-content/themes/municipio-highschool/library/Controller/Page.php
+++ wp-content/themes/municipio-highschool/library/Controller/Page.php
@@ -4,11 +4,32 @@ namespace Municipio\Controller;
 
 class Page extends \Municipio\Controller\BaseController
 {
+
+    protected $post;
+
     public function init()
     {
+
+        $this->globalToLocal('post');
+
         $this->data['hasLeftSidebar'] = false;
         $this->data['contentGridSize'] = 'grid-md-7';
 
-        $this->data['menuTitle'] = "Test heading";
+        $this->data['menuTitle'] = $this->getMenuName();
+
+
+
+    }
+
+    public function getTopLevelPageID()
+    {
+
+        var_dump(get_post_ancestors($this->post));
+
+    }
+
+    public function getMenuName()
+    {
+        $this->getTopLevelPageID();
     }
 }
