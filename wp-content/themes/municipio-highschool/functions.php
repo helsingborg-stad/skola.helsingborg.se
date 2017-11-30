05c53a9e4f61a6b7391e0cd812275db969cd54d5
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-11-30 16:15:31 +0100

    Add content background

--- wp-content/themes/municipio-highschool/functions.php
+++ wp-content/themes/municipio-highschool/functions.php
@@ -11,10 +11,22 @@ add_action('after_setup_theme', function () {
     load_theme_textdomain('municipio-school', get_template_directory() . '/languages');
 });
 
+
 require_once MUNICIPIOHIGHSCHOOL_PATH . 'library/Vendor/Psr4ClassLoader.php';
+
 $loader = new MunicipioHighSchool\Vendor\Psr4ClassLoader();
 $loader->addPrefix('MunicipioHighSchool', MUNICIPIOHIGHSCHOOL_PATH . 'library');
 $loader->addPrefix('MunicipioHighSchool', MUNICIPIOHIGHSCHOOL_PATH . 'source/php/');
 $loader->register();
+
+add_action('init', function () {
+    $acfExportManager = new \AcfExportManager\AcfExportManager();
+    $acfExportManager->setTextdomain('municipio-school');
+    $acfExportManager->setExportFolder(MUNICIPIOHIGHSCHOOL_PATH . 'library/AcfFields');
+    $acfExportManager->autoExport(array(
+        'content-background' => 'group_5a1ed4964d883',
+    ));
+    $acfExportManager->import();
+});
 
 new MunicipioHighSchool\App();
---
+++ wp-content/themes/municipio-highschool/library/AcfFields/php/content-background.php
@@ -0,0 +1,47 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5a1ed4964d883',
+    'title' => __('Content background', 'municipio-school'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5a1ed4b30cdc6',
+            'label' => __('Grey content background', 'municipio-school'),
+            'name' => 'content_background_grey',
+            'type' => 'true_false',
+            'value' => NULL,
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'message' => '',
+            'default_value' => 0,
+            'ui' => 0,
+            'ui_on_text' => '',
+            'ui_off_text' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'options_page',
+                'operator' => '==',
+                'value' => 'acf-options-theme-options',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
