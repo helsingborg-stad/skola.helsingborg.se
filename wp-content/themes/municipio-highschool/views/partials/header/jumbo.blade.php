55f9a6b7f7ff56baa53b855382b2cec1737a6cb7
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-12-01 09:32:44 +0100

    new markup, mobilefirst style @ top sticky navbar

---
+++ wp-content/themes/municipio-highschool/views/partials/header/jumbo.blade.php
@@ -0,0 +1,50 @@
+<div class="search-top {!! apply_filters('Municipio/desktop_menu_breakpoint', 'hidden-sm'); !!} hidden-print" id="search">
+    <div class="container">
+        <div class="grid">
+            <div class="grid-sm-12">
+                {{ get_search_form() }}
+            </div>
+        </div>
+    </div>
+</div>
+
+<nav class="navbar navbar-mainmenu hidden-print {{ is_front_page() && get_field('header_transparent', 'option') ? 'navbar-transparent' : '' }} {{ get_field('header_sticky', 'option') ? apply_filters( 'Municipio/StickyScroll', 'sticky-scroll' ) : '' }}">
+    <div class="container">
+        <div class="nav-wrapper">
+            <!-- Brand -->
+            <div class="brand">
+                {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
+            </div>
+
+            <!-- Navigation links -->
+            @if (get_field('nav_primary_enable', 'option') === true)
+                <nav class="{!! apply_filters('Municipio/Jumbo/NavGroupClass', 'nav-group-overflow'); !!} hidden-xs hidden-sm" data-btn-width="100">
+                    {!! $navigation['mainMenu'] !!}
+
+                    <span class="dropdown">
+                        <span class="btn btn-primary dropdown-toggle hidden"><?php _e('More', 'municipio'); ?></span>
+                        <ul class="dropdown-menu nav-grouped-overflow hidden"></ul>
+                    </span>
+
+                    @if (get_field('header_dropdown_links', 'option') === true && \Municipio\Helper\Navigation::getMenuNameByLocation('dropdown-links-menu'))
+                        <span class="dropdown hidden-xs hidden-sm hidden-md">
+                            <span class="btn btn-primary dropdown-toggle dropdown-links-menu">{{ \Municipio\Helper\Navigation::getMenuNameByLocation('dropdown-links-menu')}}</span>
+                            {!! \Municipio\Theme\Navigation::outputDropdownLinksMenu() !!}
+                        </span>
+                    @endif
+                </nav>
+            @endif
+            <!-- Mobile menu -->
+            @if (strlen($navigation['mobileMenu']) > 0)
+            <a href="#mobile-menu" data-target="#mobile-menu" class="{!! apply_filters('Municipio/mobile_menu_breakpoint', 'hidden-md hidden-lg'); !!} menu-trigger"><span class="menu-icon"></span></a>
+            @endif
+
+        </div>
+    </div>
+</nav>
+
+@if (strlen($navigation['mobileMenu']) > 0)
+    <nav id="mobile-menu" class="nav-mobile-menu nav-toggle-expand nav-toggle {!! apply_filters('Municipio/mobile_menu_breakpoint', 'hidden-md hidden-lg'); !!} hidden-print">
+        @include('partials.mobile-menu')
+    </nav>
+@endif
