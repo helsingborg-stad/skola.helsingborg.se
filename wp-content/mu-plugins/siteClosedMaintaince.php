f7b0148a3e13128bd16d17473e621f0c4ff164c6
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-12-04 08:20:36 +0100

    Do not affect wp-admin

--- wp-content/mu-plugins/siteClosedMaintaince.php
+++ wp-content/mu-plugins/siteClosedMaintaince.php
@@ -1,8 +1,10 @@
 <?php
 
-    if (!isset($_GET['test'])) {
-        if(in_array($_SERVER['HTTP_HOST'], array("skola.helsingborg.se"))) {
-            include __DIR__ . '/splash.html';
-            exit;
+    if (strpos($_SERVER['REQUEST_URI'], "wp-admin") === false) {
+        if (!isset($_GET['test'])) {
+            if (in_array($_SERVER['HTTP_HOST'], array("skola.helsingborg.se"))) {
+                include __DIR__ . '/splash.html';
+                exit;
+            }
         }
     }
