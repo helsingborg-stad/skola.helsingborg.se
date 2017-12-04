96a6a9e0f20fd1881778784c0b4c61d60acbcd82
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-12-04 09:45:38 +0100

    Publish nicolai

--- wp-content/mu-plugins/siteClosedMaintaince.php
+++ wp-content/mu-plugins/siteClosedMaintaince.php
@@ -2,7 +2,7 @@
 
     if (strpos($_SERVER['REQUEST_URI'], "wp-admin") === false) {
         if (!isset($_GET['test'])) {
-            if (in_array($_SERVER['HTTP_HOST'], array("skola.helsingborg.se", "nicolaiskolan.helsingborg.se"))) {
+            if (in_array($_SERVER['HTTP_HOST'], array("skola.helsingborg.se"))) {
                 include __DIR__ . '/splash.html';
                 exit;
             }
