7968ed003e6d3e44f9412f6c6574697ac7b4fc0b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-04-06 16:02:49 +0200

    Load styleguide development version when ?styleguide applied to url.

---
+++ wp-content/mu-plugins/loadStyleguide.php
@@ -0,0 +1,16 @@
+<?php 
+
+<?php
+
+if(isset($_GET['styleguide'])) {
+  add_action('wp_enqueue_scripts', function() {
+    wp_enqueue_style('styleguide-local', 'https://styleguide.local.test/assets/dist/css/styleguide-css.min.css'); 
+    wp_enqueue_script('styleguide-js', 'https://styleguide.local.test/assets/dist/js/styleguide-js.min.js');
+  }, 2); 
+
+
+  add_action('wp_enqueue_scripts', function() {
+    wp_dequeue_style('styleguide-css'); 
+    //wp_dequeue_script('styleguide-js'); 
+  }, 15); 
+}
