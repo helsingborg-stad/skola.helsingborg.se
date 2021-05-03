2f5eba7650ecab0f5afb9834d3073b68db0915ec
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-05-03 14:46:11 +0200

    Add modifier & radius modifications

--- wp-content/mu-plugins/set-colors-mod.php
+++ wp-content/mu-plugins/set-colors-mod.php
@@ -15,10 +15,11 @@ secondary light,
 if(class_exists('WP_CLI')) {
     $prefix = 'customizer' . ' ';
     \WP_CLI::add_command($prefix . 'set-colors-mod', 'setPrimary');
+    \WP_CLI::add_command($prefix . 'set-radius-mod', 'setRadius');
+    \WP_CLI::add_command($prefix . 'set-modifier-mod', 'setModifier');
 }
 
 function getColors() {
-    
     
     $colors = [
         'red' => [
@@ -69,15 +70,87 @@ function setPrimary() {
     $sites = get_sites();
 
     foreach($sites as $site) {
-        switch_to_blog( $site->blog_id );   
         
-        $sitePrimaryColor = get_option( 'options_school-primary-color' );
-        $schemeName = get_option('options_color_scheme');
+        //Switch to blog
+        switch_to_blog( $site->blog_id );   
+
+        //Get primary colors
+        $sitePrimaryColor   = get_option( 'options_school-primary-color' );
+        $schemeName         = get_option('options_color_scheme');
+
+        //Create color array
         $colorScheme = $colors[$schemeName];
         $colorScheme['field_60361bcb76325'] = $sitePrimaryColor;
+
+        //Print log
+        \WP_CLI::log("Setting theme color '". $schemeName ."' on " . $site->domain . " with primary color " . $sitePrimaryColor);
     
+        //Execute
         set_theme_mod('colors', $colorScheme); 
 
+        //Restore
+        restore_current_blog(); 
+    }
+}
+
+
+function setRadius() {
+
+    $sites = get_sites();
+
+    foreach($sites as $site) {
+        
+        //Switch to blog
+        switch_to_blog( $site->blog_id );   
+
+        //Print log
+        \WP_CLI::log("Setting radius on " . $site->domain);
+    
+        //Execute
+        set_theme_mod('radius', [
+            "field_603662f7a16f8" => "2",
+            "field_6038fa31cfac6" => "4",
+            "field_6038fa400384b" => "4",
+            "field_6038fa52576ba" => "4"
+        ]); 
+
+        //Restore
+        restore_current_blog(); 
+    }
+}
+
+
+function setModifier() {
+    $sites = get_sites();
+
+    foreach($sites as $site) {
+        
+        //Switch to blog
+        switch_to_blog( $site->blog_id );   
+
+        //Print log
+        \WP_CLI::log("Setting module modifiers on " . $site->domain);
+    
+        //Execute
+        set_theme_mod('modules', [
+            "field_6061d864c6873" => "highlight",
+            "field_6062fd67a2eb4" => "accented",
+            "field_60631bb52591c" => "none",
+            "field_6063008d5068a" => "none",
+            "field_606300da5068b" => "none",
+            "field_6063013a5068c" => "none",
+            "field_6063072c25917" => "none",
+            "field_60631b4025918" => "none",
+            "field_60631b5f25919" => "none",
+            "field_60641a8df5290" => "none",
+            "field_60643b600cf66" => "none",
+            "field_6064452900410" => "none",
+            "field_607843a6ba55e" => "none",
+            "field_607843cdba55f" => "none",
+            "field_607ff0d6b8426" => "accented"
+        ]); 
+
+        //Restore
         restore_current_blog(); 
     }
 }
